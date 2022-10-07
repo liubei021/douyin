ddbb680b362e71ad25cddcd18d8be7a34874103d
allen <allen420318@gmail.com>
2022-10-07 16:25:01 +0800

    init

---
+++ pages/registerLogin/registerLogin.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="page">
+		<view>
+			<text class="code-text">Note: ~ If you already have an account, you can log in directly~</text>
+		</view>
+		<view @click="register()" class="register-button">
+			<button type="warn">register</button>
+		</view>
+		<view @click="login()">
+			<button type="primary">Log in</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	var app = getApp();
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onShow() {
+			getApp().getUserIsDisable();
+		},
+		methods: {
+			register() {
+				uni.navigateTo({
+					animationType: "fade-in",
+					url: "../register/register"
+				})
+			},
+			
+			login() {
+				uni.navigateTo({
+					animationType: "fade-in",
+					url: "../loginRegist/loginRegist"
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+.page {
+	margin-top: 60%;
+}
+.code-text {
+	font-weight: 300;
+	font-size: 34rpx;
+	color: #808080;
+	margin-top: 20rpx;
+	margin-left: 20rpx;
+}
+.register-button{
+	margin-top: 1%;
+}
+</style>
